9e5a2ed4a28001a2cc9abac34820e708153cd026
AndyWaiHim <andy.lam@centrale-marseille.fr>
2022-06-14 13:07:46 +0200

    add usedapp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import "./index.css";
+import { DAppProvider, Mainnet, Rinkeby } from "@usedapp/core";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const INFURA_PROJECT_ID = "23517455f88747ce9635ad1ceb05b323";
+const config = {
+  // networks: Rinkeby,
+  // readOnlyChainId: Rinkeby.chainId, 
+  readOnlyUrls: {
+    [Mainnet.chainId]: "https://mainnet.infura.io/v3/" + INFURA_PROJECT_ID,
+    [Rinkeby.chainId]: "https://rinkeby.infura.io/v3/" + INFURA_PROJECT_ID,
+
+  },
+}
 root.render(
   <React.StrictMode>
-    <App />
+    <DAppProvider config={config}>
+      <App />
+    </DAppProvider>
   </React.StrictMode>
 );
 
